da74affb1607c82f5a6f2dfcbe1cc769aabe905a
ag-vectivus <a.s.galecki@gmail.com>
2021-10-04 11:46:41 +0200

    Set up basic environment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,34 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Portfolio",
+    siteUrl: `https://www.yourdomain.tld`,
+    title: `Portfolio`,
+    description: `My Frontend Portfolio Website`,
+    author: `Artur Gałecki https://github.com/asgalecki`,
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Baumans:400,700`, `Coda:400`],
+        display: 'swap',
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
 };
